703123e03faf40dc23e207755ff961d0fe66c9f0
huy hoang <hmhuy191101@gmail.com>
2022-11-04 22:36:27 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Meme from "./meme";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { images: [] };
+    this.getImages();
+    this.containerImage = React.createRef();
+  }
+  getImages = async () => {
+    this.setState({images: []})
+    const response = await Meme.get("/get_memes");
+    this.setState({ images: response.data.data.memes });
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  render() {
+    return (
+      <div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <button onClick={this.getImages}>Load</button>
+        </div>
+        <div
+          ref={this.containerImage}
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            margin: "30px 160px 0px",
+          }}
+        >
+          {this.state.images.map((image) => (
+            <div
+              style={{
+                width: "200px",
+                display: "inline-flex",
+                flexDirection: "column",
+                margin: "20px 10px 0px",
+              }}
+            >
+              <img
+                style={{
+                  width: "200px",
+                  height: "200px",
+                  border: "1px solid black",
+                }}
+                alt={image.name}
+                key={image.id}
+                src={image.url}
+              />
+              <div
+                style={{
+                  display: "flex",
+                  textAlign: "center",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                {image.name}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
